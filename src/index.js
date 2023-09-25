8a5b8675052e542b97d777fa41db6114cb906c44
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-09-25 09:34:44 -0400

    bump version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 //bomberfish
-
 class KVAdapter {
 	ns;
 
@@ -749,7 +748,7 @@ const project = {
 	name: 'Infrared Bare Server',
 	description: 'TompHTTP-compliant proxy server running on Cloudflare Workers.',
 	website: 'https://infrared.bomberfish.workers.dev',
-	version: '1.2.2'
+	version: '0.0.1'
 };
 function json(status, json) {
 	return new Response(JSON.stringify(json, null, '\t'), {
